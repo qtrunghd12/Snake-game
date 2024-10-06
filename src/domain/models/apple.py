8d9c70ba528106ba594b703ddb56a009e2adec10
qtrunghd12 <nsnd1357@gmail.com>
2024-10-06 18:32:48 +0700

    Sửa lỗi táo có thể xuất hiện trong tường

--- src/domain/models/apple.py
+++ src/domain/models/apple.py
@@ -1,8 +1,9 @@
-from typing import Tuple
+from typing import Tuple, List
 from random import randrange
 
 from ..interfaces import Drawable, Displayable, Position
 from ..constants import *
+from adapters.gui import snake_gui  # Import snake_gui
 
 
 class Apple(Drawable):
@@ -17,8 +18,18 @@ class Apple(Drawable):
     def generate_random_coordinates() -> Tuple[int, int]:
         return randrange(1, COL_COUNT - 1), randrange(1, ROW_COUNT-1)
 
-    def set_random_position(self):                    # change apple position
-        self.x, self.y = self.generate_random_coordinates()
+    def set_random_position(self, walls: List[Position]):                    # change apple position
+        while True:
+            self.x, self.y = self.generate_random_coordinates()
+
+            # Check if the new apple position collides with any wall
+            apple_position = Position(
+                coordinates=(self.x * CELL_SIZE, self.y * CELL_SIZE),
+                dimensions=(self.size, self.size)
+            )
+
+            if not any(snake_gui.rectangles_collide(apple_position, wall) for wall in walls):
+                break  # Valid position found
 
         if Apple.count % 3 == 0:    # each 3d apple is going to be bigger
             self.size = CELL_SIZE + 6
