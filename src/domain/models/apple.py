257d1dcf15c985c2a8372df28a5d389f41f21374
qtrunghd12 <nsnd1357@gmail.com>
2024-10-08 22:33:47 +0700

    Việt hóa chú thích

--- src/domain/models/apple.py
+++ src/domain/models/apple.py
@@ -18,18 +18,18 @@ class Apple(Drawable):
     def generate_random_coordinates() -> Tuple[int, int]:
         return randrange(1, COL_COUNT - 1), randrange(1, ROW_COUNT-1)
 
-    def set_random_position(self, walls: List[Position]):                    # change apple position
+    def set_random_position(self, walls: List[Position]):                    # walls là danh sách các tường
         while True:
             self.x, self.y = self.generate_random_coordinates()
 
-            # Check if the new apple position collides with any wall
+             # Tạo đối tượng Position của quả táo để kiểm tra
             apple_position = Position(
                 coordinates=(self.x * CELL_SIZE, self.y * CELL_SIZE),
                 dimensions=(self.size, self.size)
             )
-
+             # Kiểm tra xem có va chạm với tường không
             if not any(snake_gui.rectangles_collide(apple_position, wall) for wall in walls):
-                break  # Valid position found
+                break  # Nếu không va chạm, dừng lại
 
         if Apple.count % 3 == 0:    # each 3d apple is going to be bigger
             self.size = CELL_SIZE + 6
